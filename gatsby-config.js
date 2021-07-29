94d6ed3a2a3c6a3a73e1fd28c5204161395d4d59
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 12:06:22 +0000

    gatsby-config.js: changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
 				path: `${__dirname}/src/images`
 			}
 		},
-        		`gatsby-transformer-sharp`,
+        `gatsby-transformer-sharp`,
 		`gatsby-plugin-sharp`,
 		`gatsby-plugin-netlify-cms`,
         `gatsby-transformer-remark`,
